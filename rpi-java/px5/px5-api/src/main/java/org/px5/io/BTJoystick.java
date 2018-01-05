f7e3070d0fe731aef28eba578cf709e07a5aa7b8
Shawn <advulcan@users.noreply.github.com>
2018-01-05 17:13:58 +0800

    Update BTJoystick.java

--- rpi-java/px5/px5-api/src/main/java/org/px5/io/BTJoystick.java
+++ rpi-java/px5/px5-api/src/main/java/org/px5/io/BTJoystick.java
@@ -1,5 +1,6 @@
 package org.px5.io;
 
 public class BTJoystick {
-	public native void jsInput();
+	public native void jsInput(String callbackName, String callbackSignature);
+	public void refresh(int [] maps);
 }
